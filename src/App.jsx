e9584df130f6deeab376f1a63099ab78b7eeba88
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-06-15 22:32:26 +0300

    basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "@mui/material/Container";
+import React from "react"
+import MainTable from "./components/Table"
+
+const App = () => {
+
+  return (
+    <Container maxWidth="xl">
+      <MainTable />
+    </Container>
+  )
+}
+
+export default App;
